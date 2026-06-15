8e6226e946bbc6c39372c63d5b071f13dd7d838b
CaracatoP <joaogabrielcaracato@gmail.com>
2026-06-14 22:58:43 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const revealElements = document.querySelectorAll(".reveal");
+
+    function revealOnScroll() {
+        const windowHeight = window.innerHeight;
+
+        revealElements.forEach(function (element) {
+            const elementTop = element.getBoundingClientRect().top;
+
+            if (elementTop < windowHeight - 80) {
+                element.classList.add("active");
+            }
+        });
+    }
+
+    revealOnScroll();
+    window.addEventListener("scroll", revealOnScroll);
+});
